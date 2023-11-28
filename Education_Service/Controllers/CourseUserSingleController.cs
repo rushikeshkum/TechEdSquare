d4544e6c843745b12b197228cd166e32e862a5f8
Rushikesh Kumbhar <rushikeshkumbhar151@gmail.com>
2023-11-28 18:55:36 +0530

    minor changess in navbar course show dinmacaly fro user side ui add of my course anduser profile and uservideo

--- Education_Service/Controllers/CourseUserSingleController.cs
+++ Education_Service/Controllers/CourseUserSingleController.cs
@@ -8,10 +8,28 @@ namespace Education_Service.Controllers
 {
     public class CourseUserSingleController : Controller
     {
-        // GET: CourseUserSingle
-        public ActionResult Index()
+        DB_techedEntities db=new DB_techedEntities();
+        
+        public ActionResult Index(int id)
         {
+            var course = db.tblClassCourses.FirstOrDefault(c => c.id == id);
+
+            if (course != null)
+            {
+                ViewBag.CourseName=course.CourseName;
+                ViewBag.CourseCategory = course.tblClassCategory.CategoryName;
+                //ViewBag.EducatorProfile=course.EducatorProfile;
+                ViewBag.CourseShortD = course.CourseDiscription;
+                ViewBag.CourseDetaildD = course.DetailedDiscription;
+                ViewBag.CourseDuration = course.CourseDuration;
+                ViewBag.CourseFees = course.CourseFees;
+                ViewBag.CourseImage = course.CourseImage;
+
+            }
+
             return View();
         }
+
+
     }
 }
---
+++ Education_Service/Controllers/MyCourseUserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education_Service.Controllers
+{
+    public class MyCourseUserController : Controller
+    {
+        // GET: MyCourseUser
+        public ActionResult MyCourse()
+        {
+            return View();
+        }
+
+        
+    }
+}
